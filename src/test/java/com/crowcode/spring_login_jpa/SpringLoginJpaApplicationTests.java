37c4b6061c12f67993e9d9451eb0d67f69c92539
Cuervo <vidalcuervo@gmail.com>
2025-10-27 10:11:53 -0600

    Proyecto base de Spring Boot inicializado

---
+++ src/test/java/com/crowcode/spring_login_jpa/SpringLoginJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowcode.spring_login_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
